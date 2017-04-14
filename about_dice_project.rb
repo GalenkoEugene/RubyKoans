0fc9ee8c2573ab643110c5549290ff7399a056fe
Eugene <Re4port@ukr.net>
2017-04-14 10:42:38 +0300

    about dice project, refactored

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -2,11 +2,11 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
  class DiceSet
+  attr_reader :values
    def roll(n)
-    @arr = []
-    n.times { @arr<<rand(1..6) }
+    @values = []
+    n.times { @values<<rand(1..6) }
    end
-   def values; @arr end
  end
 
 class AboutDiceProject < Neo::Koan
